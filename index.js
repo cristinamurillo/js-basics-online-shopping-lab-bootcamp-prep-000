b14bfb7ed42aec8e8561e59d94728c7bc0b543cf
Cristina Murillo <cmurill1@jhu.edu>
2018-07-26 20:12:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,5 +72,10 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (cardNumber===""){
     return "Sorry, we don't have a credit card on file for you."
+  } else {
+    var total = total()
+    
+    
+  }
   }
 }
