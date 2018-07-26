e72e67253d4b325b36f6130432f2eafc70df2bfe
Cristina Murillo <cmurill1@jhu.edu>
2018-07-26 20:06:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,20 +9,49 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+ var price = Math.floor((Math.random() *100) + 1)
+
+ var itemConstructor = {itemName: itemName, itemPrice: price}
+ 
+ cart.push(itemConstructor)
+ 
+ return `${itemName} has been added to your cart.`
+ 
 }
 
 function viewCart() {
-  // write your code here
+  var cartString = ""
+  var i = cart.length
+  if (i===0){
+    return "Your shopping cart is empty."
+  } else if (i===1){
+    cartString = `${cart[i-1].itemName} at $${cart[i-1].itemPrice}`
+  } else {
+  var n = 0;
+  while (i>1){
+    cartString = `${cartString}${cart[n].itemName} at $${cart[n].itemPrice}, `
+    n++
+    i--;
+   } 
+   cartString = `${cartString}and ${cart[n].itemName} at $${cart[n].itemPrice}`
+  }
+  
+  return `In your cart, you have ${cartString}.`
 }
 
 function total() {
-  // write your code here
+  var i = cart.length
+  var total = 0
+  while(i>0){
+    total = total + cart[i-1].itemPrice 
+    i--;
+  }
+  return total
 }
 
 function removeFromCart(item) {
-  // write your code here
+  
 }
 
 function placeOrder(cardNumber) {
