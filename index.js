afd8b4b4279fb5ee19167ba5a544dea2e898b218
Cristina Murillo <cmurill1@jhu.edu>
2018-07-26 20:18:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber===null){
+  if (cardNumber.length===0){
     return "Sorry, we don't have a credit card on file for you."
   } else {
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
