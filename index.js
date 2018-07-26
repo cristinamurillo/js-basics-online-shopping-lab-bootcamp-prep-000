0bef2286c5f2102ee4f27b40c19e09156efb45bb
Cristina Murillo <cmurill1@jhu.edu>
2018-07-26 20:14:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,7 +73,6 @@ function placeOrder(cardNumber) {
   if (cardNumber===""){
     return "Sorry, we don't have a credit card on file for you."
   } else {
-    var total = total()
-    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
 }
